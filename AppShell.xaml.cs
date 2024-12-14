a6dbdd5f5fa47eb8fa41af2aca02cd3b52f701b1
Oran Flanagan <G00428429@atu.ie>
2024-12-14 01:12:10 +0000

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FlanaganOranTriviaGame
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
